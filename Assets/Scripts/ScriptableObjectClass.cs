f9c5ff99f1afbb945bcd968f452acbff39a7cc48
Zhuoyue Lyu <zhuoyue.lyu@gmail.com>
2020-08-09 22:10:53 -0400

    update

---
+++ Assets/Scripts/ScriptableObjectClass.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Data", menuName = "CreateData", order = 1)]
+public class ScriptableObjectClass : ScriptableObject
+{
+    public string objectName = "New MyScriptableObject";
+    public bool colorIsRandom = false;
+    public Color thisColor = Color.white;
+    public Vector3[] spawnPoints;
+}
